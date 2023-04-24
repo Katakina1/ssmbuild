72c489768fafeb7839853ab0dbed45386426c878
Kina <12571500+katakina@user.noreply.gitee.com>
2023-04-24 21:20:22 +0800

    Initial commit

---
+++ src/main/java/com/kina/service/BookServiceImpl.java
@@ -0,0 +1,41 @@
+package com.kina.service;
+
+import com.kina.dao.BookMapper;
+import com.kina.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+    // service层调用dao层， 组合Dao
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+    @Override
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    @Override
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
